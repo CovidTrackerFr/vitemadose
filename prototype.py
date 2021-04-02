c9687bb230eedbd22dfb4e23b9a4256eeb19717d
Florent Jaby <florent.jaby@gmail.com>
2021-04-02 23:09:55 +0200

    Export par départements dans les fichiers idoines
    Plus refacto pour utiliser un pool d'executeurs
    et paralléliser les recherches sur les centres

--- prototype.py
+++ prototype.py
@@ -1,13 +1,86 @@
+from datetime import datetime
+from multiprocessing import Pool
+import json
 import os
 import io
+from itertools import islice
 import re
 import csv
 import requests
+import pandas as pd
 
+session = requests.session()
+if os.getenv('WITH_TOR') is not None:
+  session.proxies = {'http':  'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'}
 
+POOL_SIZE = int(os.getenv('POOL_SIZE', 8))
 DOCTOLIB_HEADERS = {
-    'X-Covid-Tracker-Key': os.environ.get('DOCTOLIB_API_KEY', None)
+    'X-Covid-Tracker-Key': os.environ.get('DOCTOLIB_APIKEY', None)
 }
+
+def main():
+    sauter_n_centres = 0
+    with Pool(POOL_SIZE) as pool:
+        centres_cherchés = pool.imap_unordered(
+            cherche_prochain_rdv_dans_centre,
+            islice(centre_iterator(), 0, None, 1 + sauter_n_centres),
+            1
+        )
+        export_data(centres_cherchés)
+
+def cherche_prochain_rdv_dans_centre(row):
+    start_date = datetime.now().isoformat()[:10]
+    try:
+        plateforme, next_slot = fetch_centre_slots(row['rdv_site_web'], start_date)
+    except Exception as e:
+        print(f"erreur lors du traitement de la ligne {row['gid']}")
+        print(e)
+        next_slot = None
+        plateforme = None
+
+    print(plateforme, next_slot, numero_departement(row))
+    return {
+        'departement': numero_departement(row),
+        'nom': row['nom'],
+        'url': row['rdv_site_web'],
+        'plateforme': plateforme,
+        'prochain_rdv': next_slot
+    }
+
+def export_data(centres_cherchés):
+    par_departement = {
+        code: {
+            'version': 1,
+            'last_updated': datetime.now().isoformat(),
+            'centres_disponibles': [],
+            'centres_indisponibles': []
+        }
+        for code in import_departements()
+    }
+    for centre in centres_cherchés:
+        code_departement = centre['departement']
+        if code_departement in par_departement:
+            if centre['prochain_rdv'] is None:
+                par_departement[code_departement]['centres_indisponibles'].append(centre)
+            else:
+                par_departement[code_departement]['centres_disponibles'].append(centre)
+        else:
+            print(f"WARNING: le centre {centre['nom']} ({code_departement}) n'a pas pu être rattaché à un département connu")
+
+    for code_departement, disponibilités in par_departement.items():
+        print(f'writing result to {code_departement}.json file')
+        with open(f'data/output/{code_departement}.json', "w") as outfile:
+            outfile.write(json.dumps(disponibilités, indent=2))
+
+
+def fetch_centre_slots(rdv_site_web, start_date):
+    if rdv_site_web.startswith('https://partners.doctolib.fr'):
+        return 'Doctolib', fetch_doctolib_slots(rdv_site_web, start_date)
+    if rdv_site_web.startswith('https://vaccination-covid.keldoc.com'):
+        return 'Keldoc', None
+    if rdv_site_web.startswith('https://www.maiia.com'):
+        return 'Maiia', None
+    return 'Autre', None
 
 
 def fetch_doctolib_slots(rdv_site_web, start_date):
@@ -16,7 +89,7 @@ def fetch_doctolib_slots(rdv_site_web, start_date):
         return None
 
     centre_api_url = f'https://partners.doctolib.fr/booking/{centre.group(1)}.json'
-    response = requests.get(centre_api_url, headers=DOCTOLIB_HEADERS)
+    response = session.get(centre_api_url, headers=DOCTOLIB_HEADERS)
     response.raise_for_status()
     data = response.json()
 
@@ -62,7 +135,7 @@ def fetch_doctolib_slots(rdv_site_web, start_date):
 
     slots_api_url = f'https://partners.doctolib.fr/availabilities.json?start_date={start_date}&visit_motive_ids={visit_motive_id}&agenda_ids={agenda_ids}&insurance_sector=public&practice_ids={practice_ids}&destroy_temporary=true&limit=7'
 
-    response = requests.get(slots_api_url, headers=DOCTOLIB_HEADERS)
+    response = session.get(slots_api_url, headers=DOCTOLIB_HEADERS)
     response.raise_for_status()
 
     slots = response.json()
@@ -73,19 +146,9 @@ def fetch_doctolib_slots(rdv_site_web, start_date):
     return None
 
 
-def fetch_centre_slots(rdv_site_web, start_date):
-    if rdv_site_web.startswith('https://partners.doctolib.fr'):
-        return 'Doctolib', fetch_doctolib_slots(rdv_site_web, start_date)
-    if rdv_site_web.startswith('https://vaccination-covid.keldoc.com'):
-        return 'Keldoc', None
-    if rdv_site_web.startswith('https://www.maiia.com'):
-        return 'Maiia', None
-    return 'Autre', None
-    
-
 def centre_iterator():
     url = "https://www.data.gouv.fr/fr/datasets/r/5cb21a85-b0b0-4a65-a249-806a040ec372"
-    response = requests.get(url)
+    response = session.get(url)
     response.raise_for_status()
 
     reader = io.StringIO(response.content.decode('utf8'))
@@ -94,25 +157,19 @@ def centre_iterator():
         yield row
 
 
-def fetch_all_slots(start_date):
-    output = []
-    for row in centre_iterator():
-        try:
-            plateforme, slots = fetch_centre_slots(row['rdv_site_web'], start_date)
-        except requests.exceptions.RequestException as e:
-            print(row['rdv_site_web'], e)
-            slots = None
-            plateforme = None
+def numero_departement(centre):
+    code_insee = centre['com_insee']
+    if len(code_insee) == 4:
+        code_insee = '0' + code_insee
+    if code_insee.startswith('977') or code_insee.startswith('978'):
+        return '971'
+    if code_insee.startswith('97'):
+        return code_insee[:3]
+    return code_insee[:2]
 
-        print(plateforme, slots, row['rdv_site_web'])
+def import_departements():
+    df = pd.read_csv('data/input/departements-france.csv')
+    return df.code_departement.astype(str).to_list()
 
-        output.append({
-            'nom': row['nom'],
-            'url': row['rdv_site_web'],
-            'plateforme': plateforme,
-            'prochain_rdv': slots
-        })
-    return output
-        
 
-print(fetch_all_slots('2021-04-15'))
+main()
