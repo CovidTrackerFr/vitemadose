d029e0ec167bd177c84fb6bfe78abbea05817d68
Florimond Manca <florimond.manca@gmail.com>
2021-04-03 21:45:49 +0200

    Add tests to Doctolib implementation (#17)
    * Add tests to Doctolib implementation
    
    * Reduce diff
    
    * Reduce diff

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     packages=['scraper'],
     install_requires=[
+        'httpx==0.17.1',
         'requests[socks]==2.25.1',
         'pytest==6.2.2',
     ],
---
+++ tests/test_doctolib.py
@@ -0,0 +1,196 @@
+import json
+from pathlib import Path
+
+import httpx
+from scraper.doctolib import (
+    DoctolibSlots,
+    _find_agenda_and_practice_ids,
+    _find_visit_motive_category_id,
+    _find_visit_motive_id,
+    _parse_centre,
+)
+
+
+# -- Tests de l'API (offline) --
+
+
+def test_doctolib():
+    # Cas de base.
+
+    start_date = "2021-04-03"
+    rdv_site_web = "https://partners.doctolib.fr/centre-de-vaccinations-internationales/ville1/centre1?pid=practice-165752&enable_cookies_consent=1"  # noqa
+
+    def app(request: httpx.Request) -> httpx.Response:
+        assert "X-Covid-Tracker-Key" in request.headers
+
+        if request.url.path == "/booking/centre1.json":
+            path = Path("tests", "fixtures", "doctolib", "basic-booking.json")
+            return httpx.Response(200, json=json.loads(path.read_text()))
+
+        assert request.url.path == "/availabilities.json"
+        params = dict(httpx.QueryParams(request.url.query))
+        assert params == {
+            "start_date": start_date,
+            "visit_motive_ids": "2",
+            "agenda_ids": "3",
+            "insurance_sector": "public",
+            "practice_ids": "4",
+            "destroy_temporary": "true",
+            "limit": "7",
+        }
+        path = Path("tests", "fixtures", "doctolib", "basic-availabilities.json")
+        return httpx.Response(200, json=json.loads(path.read_text()))
+
+    client = httpx.Client(transport=httpx.MockTransport(app))
+    slots = DoctolibSlots(client=client)
+
+    next_date = slots.fetch(rdv_site_web, start_date)
+    assert next_date == "2021-04-10"
+
+
+def test_doctolib_next_slot():
+    # Cas de repli : c'est surprenant, mais parfois la liste des dispos
+    # est vide, mais il y a un champ 'next_slot' qui contient la date de
+    # la prochaine visite, que l'on utilise dans ce cas.
+
+    start_date = "2021-04-03"
+    rdv_site_web = "https://partners.doctolib.fr/centre-de-vaccinations-internationales/ville1/centre1?pid=practice-165752&enable_cookies_consent=1"  # noqa
+
+    def app(request: httpx.Request) -> httpx.Response:
+        assert "X-Covid-Tracker-Key" in request.headers
+
+        if request.url.path == "/booking/centre1.json":
+            path = Path("tests", "fixtures", "doctolib", "next-slot-booking.json")
+            return httpx.Response(200, json=json.loads(path.read_text()))
+
+        assert request.url.path == "/availabilities.json"
+        path = Path("tests", "fixtures", "doctolib", "next-slot-availabilities.json")
+        return httpx.Response(200, json=json.loads(path.read_text()))
+
+    client = httpx.Client(transport=httpx.MockTransport(app))
+    slots = DoctolibSlots(client=client)
+
+    next_date = slots.fetch(rdv_site_web, start_date)
+    assert next_date == "2021-04-10"
+
+
+# -- Tests unitaires --
+
+
+def test_parse_centre():
+    # Nouvelle URL
+    url = "https://partners.doctolib.fr/centre-de-vaccinations-internationales/ville1/centre1?pid=practice-165752&enable_cookies_consent=1"  # noqa
+    assert _parse_centre(url) == "centre1"
+
+    # Ancienne URL
+    url = "https://www.doctolib.fr/centre-de-vaccinations-internationales/ville2/Centre2"  # noqa
+    _parse_centre(url) == "centre2"
+
+    # URL invalide
+    url = "https://partners.doctolib.fr/centre-de-vaccinations-internationales/ville2/"
+    assert _parse_centre(url) is None
+
+
+def test_find_visit_motive_category_id():
+    data = {
+        "data": {
+            "visit_motive_categories": [
+                {"id": 41, "name": "Professionnels de santé"},
+                {"id": 42, "name": "Non professionnels de santé"},
+            ]
+        }
+    }
+    assert _find_visit_motive_category_id(data) == 42
+
+
+def test_find_visit_motive_id():
+    # Un seul motif dispo
+    data = {
+        "data": {
+            "visit_motives": [
+                {
+                    "id": 1,
+                    "visit_motive_category_id": 42,
+                    "name": "1ère injection vaccin COVID-19 (Moderna)",
+                }
+            ]
+        }
+    }
+    assert _find_visit_motive_id(data, visit_motive_category_id=42) == 1
+
+    # Plusieurs motifs dispo => on choisit le 1er dans la liste.
+    data = {
+        "data": {
+            "visit_motives": [
+                {
+                    "id": 1,
+                    "visit_motive_category_id": 42,
+                    "name": "1ère injection vaccin COVID-19 (Pfizer/BioNTech)",
+                },
+                {"id": 2, "name": "1ère injection vaccin COVID-19 (Moderna)"},
+            ]
+        }
+    }
+    assert _find_visit_motive_id(data, visit_motive_category_id=42) == 1
+
+    # Mix avec un motif autre
+    data = {
+        "data": {
+            "visit_motives": [
+                {"id": 1, "visit_motive_category_id": 42, "name": "Autre motif"},
+                {
+                    "id": 2,
+                    "visit_motive_category_id": 42,
+                    "name": "1ère injection vaccin COVID-19 (Moderna)",
+                },
+            ]
+        }
+    }
+    assert _find_visit_motive_id(data, visit_motive_category_id=42) == 2
+
+    # Mix avec une catégorie autre
+    data = {
+        "data": {
+            "visit_motives": [
+                {
+                    "id": 1,
+                    "visit_motive_category_id": 41,
+                    "name": "1ère injection vaccin COVID-19 (Moderna)",
+                },
+                {
+                    "id": 2,
+                    "visit_motive_category_id": 42,
+                    "name": "1ère injection vaccin COVID-19 (Moderna)",
+                },
+            ]
+        }
+    }
+    assert _find_visit_motive_id(data, visit_motive_category_id=42) == 2
+
+
+def test_find_agenda_and_practice_ids():
+    data = {
+        "data": {
+            "agendas": [
+                {
+                    "id": 10,
+                    "booking_disabled": False,
+                    "visit_motive_ids_by_practice_id": {
+                        "20": [1, 2],
+                        "21": [1],
+                        "22": [2],  # => Pas inclus
+                    },
+                },
+                {
+                    "id": 11,
+                    "booking_disabled": True,  # => Pas inclus
+                    "visit_motive_ids_by_practice_id": {
+                        "23": [1, 2],
+                    },
+                },
+            ],
+        },
+    }
+    agenda_ids, practice_ids = _find_agenda_and_practice_ids(data, visit_motive_id=1)
+    assert agenda_ids == ["10"]
+    assert practice_ids == ["20", "21"]
