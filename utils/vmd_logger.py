1ecdbff0f5f096e2de90b5fe142c2d698066fcdf
Aurelian <aureliancnx@gmail.com>
2021-04-05 02:00:20 +0200

    Added basic custom formatter

--- utils/vmd_logger.py
+++ utils/vmd_logger.py
@@ -1,6 +1,28 @@
 import logging
 
 
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;21m"
+    yellow = "\x1b[33;21m"
+    red = "\x1b[31;21m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "[%(levelname)s] %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 def init_logger():
     # create logger
     logger = logging.getLogger('scraper')
@@ -8,7 +30,6 @@ def init_logger():
 
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
+    ch.setFormatter(CustomFormatter())
     logger.addHandler(ch)
     return logger
