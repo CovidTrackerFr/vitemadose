72758d6a738f113316da4b7df4beaa2ab690df64
Aurelian <aureliancnx@gmail.com>
2021-04-05 20:02:05 +0200

    Added base for Keldoc tests

--- tests/test_keldoc.py
+++ tests/test_keldoc.py
@@ -1,9 +1,29 @@
 # -- Tests de l'API (offline) --
+import json
+from pathlib import Path
+
+import httpx
+
+from scraper.keldoc.keldoc_center import KeldocCenter
 
 
 def test_keldoc():
-    pass
+    center1_url = "https://vaccination-covid.keldoc.com/centre-hospitalier-regional/lorient-56100/groupe-hospitalier-bretagne-sud-lorient-hopital-du-scorff?specialty=144"
+    center1_redirect = "https://vaccination-covid.keldoc.com/redirect/?dom=centre-hospitalier-regional&inst=lorient-56100&user=groupe-hospitalier-bretagne-sud-lorient-hopital-du-scorff&specialty=144"
 
+    def app(request: httpx.Request) -> httpx.Response:
+        if request.url.path == "/centre-hospitalier-regional/lorient-56100/groupe-hospitalier-bretagne-sud-lorient-hopital-du-scorff":
+            return httpx.Response(302, headers={
+                "Location": center1_redirect
+            })
+
+        if request.url.path == "/booking/centre1.json":
+            path = Path("tests", "fixtures", "doctolib", "next-slot-booking.json")
+            return httpx.Response(200, json=json.loads(path.read_text()))
+
+    client = httpx.Client(transport=httpx.MockTransport(app))
+    test_center_1 = KeldocCenter(base_url=center1_url, client=client)
+    test_center_1.parse_resource()
 
 def test_keldoc_motive_categories():
     pass
