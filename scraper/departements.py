f0db2340143616e4d854350ce8fcc22c8dcfdc93
Jordan Afonso <jordanafonso@Jordans-MacBook-Pro.local>
2021-04-06 17:08:45 +0200

    Refacto + ajout de tests des cas particuliers

--- scraper/departements.py
+++ scraper/departements.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from typing import List
 
 
@@ -37,42 +38,39 @@ def to_departement_number(insee_code: str) -> str:
     >>> to_departement_number('97701')  # Saint-Barthélémy
     '971'
     """
-    insee_code = _clean_insee_code(insee_code)
-
-    # Gestion des cas spéciaux pour l'outre-mer (>= 97).
-
-    if insee_code.startswith(("977", "978")):
-        # Territoires historiquement rattachés à la Guadeloupe (971) :
-        # - Saint-Barthélémy (977)
-        # - Saint-Martin (978)
-        return "971"
-
-    if insee_code.startswith("97"):
-        # Autres DOM-TOM.
-        # Ex : Saint-Pierre (La Réunion, dpt 974) = 97410 -> 974
-        return insee_code[:3]
-
-    # Cas général.
-    # Ex : Lille = 59350 -> 59
-    return insee_code[:2]
-
-
-def _clean_insee_code(insee_code: str) -> str:
-    """
-    Nettoie et valide le code INSEE.
-
-    >>> _clean_insee_code('59350')
-    '59350'
-    >>> _clean_insee_code('2401')
-    '02401'
-    """
     if len(insee_code) == 4:
         # Quand le CSV des centres de vaccinations est édité avec un tableur comme Excel,
         # il est possible que le 1er zéro soit retiré si la colonne est interprétée comme
         # un nombre (par ex 02401 devient 2401, mais on veut 02401 au complet).
-        insee_code = f"0{insee_code}"
+        insee_code = insee_code.zfill(5)
 
     if len(insee_code) != 5:
         raise ValueError(f'Code INSEE non-valide : {insee_code}')
 
-    return insee_code
+    with open("data/input/insee_to_codepostal.json") as json_file:
+        insee_to_code_postal_table = json.load(json_file)
+
+    if insee_code in insee_to_code_postal_table:
+        # Gestion des cas spéciaux pour l'outre-mer et Monaco (>= 97).
+
+        if insee_code.startswith("99138"):
+            # Monaco
+            return "98"
+
+        if insee_code.startswith(("977", "978")):
+            # Territoires historiquement rattachés à la Guadeloupe (971) :
+            # - Saint-Barthélémy (977)
+            # - Saint-Martin (978)
+            return "971"
+
+        if insee_code.startswith("97"):
+            # Autres DOM-TOM.
+            # Ex : Saint-Pierre (La Réunion, dpt 974) = 97410 -> 974
+            return insee_code[:3]
+
+        # Cas général.
+        # Ex : Lille = 59350 -> 59
+        return insee_code[:2]
+    else:
+        raise ValueError(f'Code INSEE absent de la base des codes INSEE : {insee_code}')
+
