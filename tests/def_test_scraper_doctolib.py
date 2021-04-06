7fd940089a892e0787039e37326bbbe6a7607ac1
Timothée Levillayer <timothee.levillayer@gmail.com>
2021-04-06 22:26:33 +0200

    Recherche centres Doctolib

---
+++ tests/def_test_scraper_doctolib.py
@@ -0,0 +1,10 @@
+import datetime as dt
+import json
+from scraper.departements import import_departements
+from scraper.scraper_doctolib import search
+
+from .utils import mock_datetime_now
+
+
+def test:
+	search(10)
