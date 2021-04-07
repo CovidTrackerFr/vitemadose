edc1fff75e7e381be3ab93f9dab2292d5e929daf
Aurelian <aureliancnx@gmail.com>
2021-04-07 02:13:50 +0200

    Added stats by date for departements

--- scraper/centres_stats.py
+++ scraper/centres_stats.py
@@ -10,6 +10,7 @@ from utils.vmd_logger import enable_logger_for_production
 logger = logging.getLogger('scraper')
 
 STATS_BY_DATE = 'https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/stats_by_date.json'
+STATS_BY_DATE_DEP = 'https://raw.githubusercontent.com/CovidTrackerFr/vitemadose/data-auto/data/output/stats_by_date_dep.json'
 
 
 def generate_stats_date(centres_stats):
@@ -29,6 +30,7 @@ def generate_stats_date(centres_stats):
     ctz = pytz.timezone('Europe/Paris')
     current_time = datetime.now(tz=ctz).strftime("%Y-%m-%d %H:00:00")
     if current_time in stats_data['dates']:
+        logger.info("Stats file already updated: data/output/stats_by_date.json")
         return
     data_alldep = centres_stats['tout_departement']
     stats_data['dates'].append(current_time)
@@ -36,7 +38,45 @@ def generate_stats_date(centres_stats):
     stats_data['total_centres'].append(data_alldep['total'])
 
     with open("data/output/stats_by_date.json", "w") as stat_graph_file:
-        json.dump(stats_data, stat_graph_file, indent=2)
+        json.dump(stats_data, stat_graph_file)
+    logger.info("Updated stats file: data/output/stats_by_date.json")
+
+
+def generate_stats_dep_date(centres_stats):
+    stats_data = {'dates': [],
+                  'dep_centres_disponibles': {},
+                  'dep_centres': {}
+                  }
+
+    try:
+        history_rq = requests.get(STATS_BY_DATE_DEP)
+        data = history_rq.json()
+        if data:
+            stats_data = data
+    except Exception as e:
+        logger.warning("Unable to fetch {0}: generating a template file.".format(STATS_BY_DATE_DEP))
+        pass
+    ctz = pytz.timezone('Europe/Paris')
+    current_time = datetime.now(tz=ctz).strftime("%Y-%m-%d %H:00:00")
+    if current_time in stats_data['dates']:
+        logger.info("Stats file already updated: data/output/stats_by_date_dep.json")
+        return
+    stats_data['dates'].append(current_time)
+
+    for dep in centres_stats:
+        if dep == 'tout_departement':
+            continue
+        if dep not in stats_data['dep_centres_disponibles']:
+            stats_data['dep_centres_disponibles'][dep] = []
+        if dep not in stats_data['dep_centres']:
+            stats_data['dep_centres'][dep] = []
+        dep_data = centres_stats[dep]
+        stats_data['dep_centres_disponibles'][dep].append(dep_data['disponibles'])
+        stats_data['dep_centres'][dep].append(dep_data['total'])
+
+    with open("data/output/stats_by_date_dep.json", "w") as stat_graph_file:
+        json.dump(stats_data, stat_graph_file)
+    logger.info("Updated stats file: data/output/stats_by_date_dep.json")
 
 
 def export_centres_stats(center_data='data/output/info_centres.json'):
@@ -67,6 +107,7 @@ def export_centres_stats(center_data='data/output/info_centres.json'):
     with open("data/output/stats.json", "w") as stats_file:
         json.dump(centres_stats, stats_file, indent=2)
     generate_stats_date(centres_stats)
+    generate_stats_dep_date(centres_stats)
 
 
 def get_centres_info(center_data):
