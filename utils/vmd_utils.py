24ab454a5c94bfcdb171e45b9a00e42b59397bf6
grubounet <sacha.guilhaumou@free.fr>
2021-04-10 17:39:12 +0200

    correction demandée par @Bouska

--- utils/vmd_utils.py
+++ utils/vmd_utils.py
@@ -5,5 +5,5 @@ import simplejson as jsoncode
 def urlify(s):
     s = re.sub(r"[^\w\s\-]", '', s)
     s = re.sub(r"\s+", '-', s).lower()
-    return jsoncode.dumps(unicodedata.normalize('NFKD', s).encode('ASCII', 'ignore')).strip('"')
+    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
     
