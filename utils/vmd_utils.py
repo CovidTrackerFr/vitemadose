fdc65e87d7f1b1a5753fadb8b797663a1ce2a179
grubounet <sacha.guilhaumou@free.fr>
2021-04-10 15:52:48 +0200

    correction caractères spéciaux dans fonction urlify

--- utils/vmd_utils.py
+++ utils/vmd_utils.py
@@ -1,6 +1,9 @@
 import re
+import unicodedata
+import simplejson as jsoncode
 
 def urlify(s):
-    s = re.sub(r"[^\w\s\-]", '', s).lower()
+    s = re.sub(r"[^\w\s\-]", '', s)
     s = re.sub(r"\s+", '-', s).lower()
-    return s
+    return jsoncode.dumps(unicodedata.normalize('NFKD', s).encode('ASCII', 'ignore')).strip('"')
+    
