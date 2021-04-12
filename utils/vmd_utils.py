5f9730125be040664673980c5d6873a86771f0dd
Aurelian <aureliancnx@gmail.com>
2021-04-12 11:51:00 +0200

    Fixed utils indentation x3

--- utils/vmd_utils.py
+++ utils/vmd_utils.py
@@ -106,8 +106,11 @@ def format_phone_number(_phone_number: str) -> str:
     phone_number = phone_number.replace(" ", "")
     phone_number = phone_number.replace(".", "")
 
-    if not phone_number[0] == '+':
-        phone_number = "+33" + phone_number[1:]
+    if not phone_number[0] == "+":
+        if phone_number[0] == "0":
+            phone_number = "+33" + phone_number[1:]
+        else:
+            phone_number = "+33" + phone_number
 
     return phone_number
 
