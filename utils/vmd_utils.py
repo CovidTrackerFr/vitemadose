7be12af9b6491ba6cd0131af1acee2ad40a68d88
Aurelian <aureliancnx@gmail.com>
2021-04-12 14:37:54 +0200

    hotfix doctolib

--- utils/vmd_utils.py
+++ utils/vmd_utils.py
@@ -131,6 +131,7 @@ def fix_scrap_urls(url):
                 to_remove.append(query_name)
         [query.pop(rm, None) for rm in to_remove]
         query.pop('speciality_id', None)
+        query.pop('pid', None)
         u = u._replace(query=urlencode(query, True))
         url = urlunparse(u)
     return url
