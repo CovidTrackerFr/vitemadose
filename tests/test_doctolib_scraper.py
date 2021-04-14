08463b98d969b229f4b91fe1181ebcd4f1565843
Aurelian <aureliancnx@gmail.com>
2021-04-14 14:01:05 +0200

    Added unit tests to doctolib scraper

--- tests/test_doctolib_scraper.py
+++ tests/test_doctolib_scraper.py
@@ -1,7 +1,8 @@
 import json
 from pathlib import Path
 
-from scraper.doctolib.doctolib_center_scrap import parse_doctolib_centers, get_departements, doctolib_urlify
+from scraper.doctolib.doctolib_center_scrap import parse_doctolib_centers, get_departements, doctolib_urlify, \
+    get_coordinates, center_from_doctor_dict, center_type, parse_doctolib_business_hours
 from scraper.error import BlockedByDoctolibError
 
 import httpx
@@ -17,6 +18,7 @@ from scraper.doctolib.doctolib import (
 
 # -- Tests de l'API (offline) --
 from scraper.pattern.scraper_request import ScraperRequest
+from scraper.pattern.scraper_result import GENERAL_PRACTITIONER, DRUG_STORE, VACCINATION_CENTER
 
 
 def test_doctolib_departements():
@@ -27,3 +29,40 @@ def test_doctolib_departements():
 def test_doctolib_urlify():
     url = 'FooBar 42'
     assert doctolib_urlify(url) == 'foobar-42'
+
+
+def test_center_type():
+    type = center_type('https://doctolib.fr/center/foobar-42', 'Pharmacie du centre de Saint-Malo')
+    assert type == DRUG_STORE
+    type = center_type('https://doctolib.fr/medecin/foobar-42', 'Dr Foo Bar')
+    assert type == GENERAL_PRACTITIONER
+    type = center_type('https://doctolib.fr/centres-vaccination/foobar-42', 'Centre Foo Bar')
+    assert type == VACCINATION_CENTER
+
+
+def test_business_hours():
+    place = {
+        'opening_hours': [
+            {
+                'day': 1,
+                'enabled': True,
+                'ranges': [
+                    ['12:00', '15:00'],
+                    ['16:00', '17:00'],
+                ]
+            }
+        ]
+    }
+    assert parse_doctolib_business_hours(place) == {'lundi': '12:00-15:00, 16:00-17:00'}
+
+
+def test_doctolib_coordinates():
+    docto = {
+        'position': {
+            'lng': 1.381,
+            'lat': 8.192
+        }
+    }
+    long, lat = get_coordinates(docto)
+    assert long == 1.381
+    assert lat == 8.192
