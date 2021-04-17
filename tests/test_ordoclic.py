58257e94483fc04b13862727767e0564f4502018
Pablo Martin-Gomez <pablomg@eskapa.be>
2021-04-17 20:52:58 +0200

    ordoclic: implement test_parse_ordoclic_slots()

--- tests/test_ordoclic.py
+++ tests/test_ordoclic.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 import httpx
+from datetime import datetime
+from dateutil.parser import isoparse
 
 from scraper.ordoclic import (
     search,
@@ -32,7 +34,29 @@ def test_getProfile():
 
 
 def test_parse_ordoclic_slots():
-    pass
+    # Test availability_data vide
+    request = ScraperRequest("", "")
+    assert parse_ordoclic_slots(request, {}) == None
+
+    # Test pas de slots disponibles
+    empty_slots_file = Path('tests/fixtures/ordoclic/empty_slots.json')
+    empty_slots = json.loads(empty_slots_file.read_text())
+    request = ScraperRequest("", "")
+    assert parse_ordoclic_slots(request, empty_slots) == None
+
+    # Test nextAvailableSlotDate
+    nextavailable_slots_file = Path('tests/fixtures/ordoclic/nextavailable_slots.json')
+    nextavailable_slots = json.loads(nextavailable_slots_file.read_text())
+    request = ScraperRequest("", "")
+    assert parse_ordoclic_slots(request, nextavailable_slots) == isoparse("2021-06-12T11:30:00Z")  # timezone CET
+
+    # Test slots disponibles
+    full_slots_file = Path('tests/fixtures/ordoclic/full_slots.json')
+    full_slots = json.loads(full_slots_file.read_text())
+    request = ScraperRequest("", "")
+    first_availability = parse_ordoclic_slots(request, full_slots)
+    assert first_availability == isoparse("2021-04-19T16:15:00Z")  # timezone CET
+    assert request.appointment_count == 42
 
 
 def test_fetch_slots():
