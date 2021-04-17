962702f2112fbbd43fd742b1760db6235d993f95
Pablo Martin-Gomez <pablomg@eskapa.be>
2021-04-17 20:20:54 +0200

    ordoclic: add empty test module

---
+++ tests/test_ordoclic.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+import httpx
+
+from scraper.ordoclic import (
+    search,
+    getReasons,
+    getSlots,
+    getProfile,
+    parse_ordoclic_slots,
+    fetch_slots,
+    centre_iterator
+)
+
+from scraper.pattern.scraper_request import ScraperRequest
+
+
+def test_search():
+    pass
+
+
+def test_getReasons():
+    pass
+
+
+def test_getSlots():
+    pass
+
+
+def test_getProfile():
+    pass
+
+
+def test_parse_ordoclic_slots():
+    pass
+
+
+def test_fetch_slots():
+    pass
+
+
+def test_centre_iterator():
+    pass
