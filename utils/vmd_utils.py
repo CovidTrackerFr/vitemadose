eb9a0d8ba3522ffbf5c6a7849e155efb6c00a576
Maijin <maijin@reverseshade.com>
2021-04-30 16:31:46 +0200

    Revert "Doctolib - Calcul des disponibilités à 24h et 7j #265"
    This reverts commit aa6dd82295d718f890610b9a03bc1cf30cdddf4a.

--- utils/vmd_utils.py
+++ utils/vmd_utils.py
@@ -8,9 +8,6 @@ import datetime as dt
 import pytz
 import requests
 
-import time
-from datetime import date, timedelta, datetime
-
 from pathlib import Path
 from unidecode import unidecode
 
@@ -211,8 +208,3 @@ def get_last_scans(centres):
             centre.last_scan_with_availabilities = dt.datetime.now(tz=pytz.timezone('Europe/Paris')).isoformat()
 
     return liste_centres
-
-def date_plus_day(mydate, days):
-    mydate = date.fromisoformat(mydate)
-    newdate = mydate + timedelta(days = days)
-    return newdate.isoformat()
