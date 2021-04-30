a0841f41f2f465051cc618ee051f19bf4de6d6e1
Maijin <maijin@reverseshade.com>
2021-04-30 16:31:56 +0200

    Revert "Doctolib - Calcul des disponibilités à 24h et 7j #265" (#277)
    This reverts commit aa6dd82295d718f890610b9a03bc1cf30cdddf4a.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -166,7 +166,7 @@ def export_data(centres_cherchés, outpath_format='data/output/{}.json'):
             continue
         erreur = centre.erreur
         centres_open_data.append(copy_omit_keys(centre.default(), ['prochain_rdv', 'internal_id', 'metadata',
-                                                                   'location', 'appointment_count', 'appointment_schedules', 'erreur',
+                                                                   'location', 'appointment_count', 'erreur',
                                                                    'ville', 'type', 'vaccine_type',
                                                                    'appointment_by_phone_only', 'last_scan_with_availabilities']))
         if centre.prochain_rdv is None or centre.appointment_count == 0:
