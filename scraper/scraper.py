cbad149c53d9e2c5e9e291109561d8644ec74c3d
Noé <noe.pion@gmail.com>
2021-05-09 10:34:42 +0200

    remove duplicate code

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -323,15 +323,5 @@ def get_blocklist_urls() -> set:
     centers_blocklist_urls = set([center["url"] for center in json.load(open(path_blocklist))["centers_not_displayed"]])
     return centers_blocklist_urls
 
-def is_in_blocklist(center: CenterInfo, blocklist_urls) -> bool:
-    return center.url in blocklist_urls
-
-
-def get_blocklist_urls() -> set:
-    path_blocklist = "data/input/centers_blocklist.json"
-    centers_blocklist_urls = set([center["url"] for center in json.load(open(path_blocklist))["centers_not_displayed"]])
-    return centers_blocklist_urls
-
-
 if __name__ == "__main__":  # pragma: no cover
     main()
