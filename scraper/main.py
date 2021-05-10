9dd61983c29f2af4c5f327d4288aa53e342a843d
Thomas R.​ <kiwiiii@gmail.com>
2021-05-10 23:53:47 +0200

    scraper: add --url-file for a list of urls to scrap
    The file must contain one url per line.

--- scraper/main.py
+++ scraper/main.py
@@ -9,12 +9,15 @@ def main():  # pragma: no cover
     parser = argparse.ArgumentParser()
     parser.add_argument("--platform", "-p", help="scrape platform. (eg: doctolib,keldoc or all)")
     parser.add_argument("--url", "-u", action="append", help="scrape one url, can be repeated")
+    parser.add_argument("--url-file", type=argparse.FileType("r"),  help="scrape urls listed in file (one per line)")
     parser.add_argument("--merge", "-m", help="merge platform results", action="store_true")
     args = parser.parse_args()
 
     if args.merge:
         merge_platforms()
         return
+    if args.url_file:
+        args.url = [line.rstrip() for line in args.url_file]
     if args.url:
         scrape_debug(args.url)
         return
