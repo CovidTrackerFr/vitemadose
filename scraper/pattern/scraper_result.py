216f8daa30a8ab590f288fd8de7f33b4dead79d8
Pablo Martin-Gomez <pablomg@eskapa.be>
2021-05-10 22:37:45 +0200

    pattern: add annotations to scraper_result

--- scraper/pattern/scraper_result.py
+++ scraper/pattern/scraper_result.py
@@ -1,20 +1,21 @@
 import json
 from enum import Enum
+from typing import List
 
 from scraper.pattern.scraper_request import ScraperRequest
 
 
 # Practitioner type enum
-GENERAL_PRACTITIONER = "general-practitioner"
-VACCINATION_CENTER = "vaccination-center"
-DRUG_STORE = "drugstore"
+GENERAL_PRACTITIONER: str = "general-practitioner"
+VACCINATION_CENTER: str = "vaccination-center"
+DRUG_STORE: str = "drugstore"
 
 
 class ScraperResult:
-    def __init__(self, request: ScraperRequest, platform, next_availability):
-        self.request = request
-        self.platform = platform
-        self.next_availability = next_availability
+    def __init__(self, request: ScraperRequest, platform: str, next_availability: str):
+        self.request: ScraperRequest = request
+        self.platform: str = platform
+        self.next_availability: str = next_availability
 
-    def default(self):
+    def default(self) -> dict:
         return self.__dict__
