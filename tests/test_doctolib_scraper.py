271b33076e3aa5ce189157337ab6570ef8c4d7bb
Jordan Afonso <afonso.jordan11@gmail.com>
2021-05-10 16:50:04 +0200

    delete unused var

--- tests/test_doctolib_scraper.py
+++ tests/test_doctolib_scraper.py
@@ -1,30 +1,13 @@
-import json
-from pathlib import Path
-
 from scraper.doctolib.doctolib_center_scrap import (
-    parse_doctolib_centers,
     get_departements,
     doctolib_urlify,
     get_coordinates,
-    center_from_doctor_dict,
     center_type,
     parse_doctolib_business_hours,
 )
-from scraper.error import BlockedByDoctolibError
 
-import httpx
-from scraper.doctolib.doctolib import (
-    DoctolibSlots,
-    _find_agenda_and_practice_ids,
-    _find_visit_motive_category_id,
-    _find_visit_motive_id,
-    _parse_centre,
-    _parse_practice_id,
-    DOCTOLIB_SLOT_LIMIT,
-)
 
 # -- Tests de l'API (offline) --
-from scraper.pattern.scraper_request import ScraperRequest
 from scraper.pattern.scraper_result import GENERAL_PRACTITIONER, DRUG_STORE, VACCINATION_CENTER
 
 
