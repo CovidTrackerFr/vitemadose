6fae04b75e0181b9e2c121d7282f3b272a34f3c8
Pablo Martin-Gomez <pablomg@eskapa.be>
2021-05-10 22:33:29 +0200

    pattern: add annotations to scraper_request

--- scraper/pattern/scraper_request.py
+++ scraper/pattern/scraper_request.py
@@ -1,37 +1,38 @@
 import hashlib
-from typing import Optional
+from typing import Optional, List
+from scraper.center_info import Vaccine
 
 
 class ScraperRequest:
     def __init__(self, url: str, start_date: str):
-        self.url = url
-        self.start_date = start_date
-        self.internal_id = None
-        self.practitioner_type = None
-        self.appointment_count = 0
-        self.appointment_schedules = None
-        self.vaccine_type = None
-        self.appointment_by_phone_only = False
+        self.url: str = url
+        self.start_date: str = start_date
+        self.internal_id: Optional[str] = None
+        self.practitioner_type: Optional[str] = None
+        self.appointment_count: Optional[int] = 0
+        self.appointment_schedules: Optional[List] = None
+        self.vaccine_type: Optional[List[Vaccine]] = None
+        self.appointment_by_phone_only: bool = False
 
-    def update_internal_id(self, internal_id: str) -> Optional[str]:
+    def update_internal_id(self, internal_id: Optional[str]) -> Optional[str]:
         self.internal_id = internal_id
         return self.internal_id
 
-    def update_practitioner_type(self, practitioner_type: str) -> Optional[str]:
+    def update_practitioner_type(self, practitioner_type: Optional[str]) -> Optional[str]:
         self.practitioner_type = practitioner_type
         return self.practitioner_type
 
-    def update_appointment_count(self, appointment_count: int) -> Optional[int]:
+    def update_appointment_count(self, appointment_count: Optional[int]) -> Optional[int]:
         self.appointment_count = appointment_count
         return self.appointment_count
 
-    def update_appointment_schedules(self, appointment_schedules: dict):
+    def update_appointment_schedules(self, appointment_schedules: Optional[list]):
         self.appointment_schedules = appointment_schedules
 
-    def get_appointment_schedules(self) -> list:
+    def get_appointment_schedules(self) -> Optional[List]:
         return self.appointment_schedules
 
-    def add_vaccine_type(self, vaccine_name):
+    def add_vaccine_type(self, vaccine_name: Optional[Vaccine]):
         if not vaccine_name:
             return
         if self.vaccine_type is None:
@@ -46,5 +47,5 @@ class ScraperRequest:
     def get_start_date(self) -> str:
         return self.start_date
 
-    def set_appointments_only_by_phone(self, only_by_phone: bool) -> None:
+    def set_appointments_only_by_phone(self, only_by_phone: bool):
         self.appointment_by_phone_only = only_by_phone
