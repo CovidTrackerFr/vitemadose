f1df5b1725471929fea3a7d313092a7e4e103808
Valentin Mouret <valentin.mou@gmail.com>
2021-05-11 22:57:03 +0200

    Add a dev folder with tools
    The intent of this folder is to have tools useful to explore
    the data and run checks.
    
    Spin a REPL and start repling!

---
+++ dev/model/schedule.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class Schedule(BaseModel):
+    name: str  # "chronodoses"
+    from_: datetime  # "2021-05-10T00:00:00+02:00"
+    to: datetime  # "2021-05-11T23:59:59+02:00"
+    total: int
+
+    def __init__(self, **kwargs):
+        args = kwargs
+        if "from" in args:
+            args["from_"] = args.pop("from")
+        super().__init__(**args)
