ca28f8b0f8f96d61120fca8a46d5c64582cfdcc4
Jordan Afonso <afonso.jordan11@gmail.com>
2021-05-11 18:12:15 +0200

    add test to parse_page_centers()

--- tests/test_doctolib_scraper.py
+++ tests/test_doctolib_scraper.py
@@ -7,9 +7,11 @@ from scraper.doctolib.doctolib_center_scrap import (
     get_pid,
     find_place,
     get_dict_infos_center_page,
+    parse_page_centers,
 )
 
 import requests
+import json
 # -- Tests de l'API (offline) --
 from scraper.pattern.scraper_result import GENERAL_PRACTITIONER, DRUG_STORE, VACCINATION_CENTER
 
@@ -56,6 +58,7 @@ def test_business_hours():
     assert parse_doctolib_business_hours(place) == {"lundi": "12:00-15:00, 16:00-17:00", "mardi": None}
     assert parse_doctolib_business_hours(emptyPlace) == None
 
+
 def test_doctolib_coordinates():
     docto = {"position": {"lng": 1.381, "lat": 8.192}}
     long, lat = get_coordinates(docto)
@@ -72,26 +75,26 @@ def test_doctolib_get_pid():
 
 def test_doctolib_find_place():
     places = [
-            {
-                "id": "0",
-                "name": "someone"
-            },
-            {
-                "id": "1",
-                "name": "like you"
-            },
-        ]
+        {
+            "id": "0",
+            "name": "someone"
+        },
+        {
+            "id": "1",
+            "name": "like you"
+        },
+    ]
     urlWithPid = "someURL?pid=1"
     urlWithoutPid = "someURL"
     assert find_place(places, urlWithoutPid)["name"] == "someone";
     assert find_place(places, urlWithPid)["name"] == "like you";
 
+
 from unittest.mock import Mock, patch
+
 
 @patch('requests.get')
 def test_get_dict_infos_center_page(mock_get):
-    import json
-
     with open("tests/fixtures/doctolib/booking.json", "r") as file:
         booking = json.load(file)
 
@@ -150,3 +153,34 @@ def test_get_dict_infos_center_page(mock_get):
     }
     mockedResponse = get_dict_infos_center_page('someURL')
     assert mockedResponse == {}
+
+
+@patch('requests.get')
+def test_parse_page_centers(mock_get):
+    with open("tests/fixtures/doctolib/doctors.json", "r") as file:
+        doctors = json.load(file)
+
+    expectedCentersPage = [
+        {
+            "nom": "Pharmacie des écoles - Leadersanté - Villejuif",
+            "ville": "Villejuif",
+            "address": "22b Rue Jean Jaurès, 94800 Villejuif",
+            "type": "drugstore",
+            "rdv_site_web":"https://www.doctolib.fr/pharmacie/villejuif/pharmacie-des-ecoles",
+            "long_coor1": 2.3662778,
+            "lat_coor1": 48.7951181,
+            "com_insee": "94076",
+        },
+        {
+            "nom": "Vaccinodrome Jean Jaurès",
+            "ville": "Le Petit-Quevilly",
+            "address": "96 Avenue Jean Jaurès, 76140 Le Petit-Quevilly",
+            "type": "vaccination-center",
+            "rdv_site_web": "https://partners.doctolib.fr/vaccinodrome/le-petit-quevilly/dubois?pid=1",
+            "long_coor1": 1.0627287,
+            "lat_coor1": 49.4269181,
+            "com_insee": "76498",
+        }]
+    mock_get.return_value.json.return_value = doctors
+    mockedResponse = parse_page_centers(0)
+    assert mockedResponse == expectedCentersPage
