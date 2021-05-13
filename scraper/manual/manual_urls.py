ad84009b16ac67d9fed88236d50b1d331aa3b3fe
MyPseudo <damien8857220@gmail.com>
2021-05-13 14:21:20 +0200

    ajout manual_urls.csv, ajoute de la méthode manual_uls_iterator() + ajout d'un dossier manual/manual_urls.py #435

---
+++ scraper/manual/manual_urls.py
@@ -0,0 +1,21 @@
+import csv
+from typing import Iterator
+
+
+from utils.vmd_logger import get_logger
+
+
+logger = get_logger()
+
+
+def manual_uls_iterator() -> Iterator[dict]:
+    logger.info("Recherche des urls manuel")
+    path_file = "data/input/manual_urls.csv"
+    total = 0
+    with open(path_file, encoding="utf8") as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";")
+        for row in reader:
+            total = total + 1
+            # row["rdv_site_web"] = fix_scrap_urls(row["rdv_site_web"])
+        logger.info("Nombre d'urls manuel " + str(total))
+        yield row
