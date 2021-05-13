766ed6a71c916b79314f3314cdfae762424486e5
Noe <8218350+Noezor@users.noreply.github.com>
2021-05-13 23:47:26 +0200

    warn > error

--- scraper/doctolib/doctolib_center_scrap.py
+++ scraper/doctolib/doctolib_center_scrap.py
@@ -174,7 +174,7 @@ def get_dict_infos_center_page(url_path: str) -> dict:
         data.raise_for_status()
         output = data.json().get("data", {})
     except:
-        logger.error(f"> Could not retrieve data from {internal_api_url}")
+        logger.warn(f"> Could not retrieve data from {internal_api_url}")
         return liste_infos_page
 
     # Parse place
@@ -269,7 +269,7 @@ if __name__ == "__main__":  # pragma: no cover
         logger.info(f"Found {len(centers)} centers on Doctolib")
         if len(centers) < 10000:
             # for reference, on 13-05, there were 12k centers
-            logger.warn(f"[NOT SAVING RESULTS]{len(centers)} does not seem like enough Doctolib centers")
+            logger.error(f"[NOT SAVING RESULTS]{len(centers)} does not seem like enough Doctolib centers")
         else:
             logger.info(f"> Writing them on {path_out}")
             with open(path_out, "w") as f:
