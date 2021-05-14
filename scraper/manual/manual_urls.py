1d53b8bfa83081796d4de9dfaf290b251641894f
Valentin Mouret <valentin.mou@gmail.com>
2021-05-14 18:43:32 +0200

    Adapt the scraping of manual URLs to use Google sheets

--- scraper/manual/manual_urls.py
+++ scraper/manual/manual_urls.py
@@ -1,21 +1,30 @@
-import csv
-from typing import Iterator
+"""Scrapes a given list of URLs.
 
+They come from a Google sheets configured in `config.json`.
+"""
 
+from typing import Dict, Iterator
+
+from pydantic import BaseModel
+
+from utils.vmd_config import get_config
 from utils.vmd_logger import get_logger
+from utils.vmd_utils import fix_scrap_urls
+from scraper import sheets
+
+
+class Config(BaseModel):
+    sheet_id: str  # ID of the spreadsheet.
+    page_number: int  # Index of the sheet inside the spreadsheet (starts at 1).
+    column_names: Dict[int, str]  # Mapping of column ID to column name (starts at 1).
 
 
 logger = get_logger()
 
+config = Config(**get_config().get("manual"))
+
 
 def manual_urls_iterator() -> Iterator[dict]:
-    logger.info("Recherche des urls manuel")
-    path_file = "data/input/manual_urls.csv"
-    total = 0
-    with open(path_file, encoding="utf8") as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=";")
-        for row in reader:
-            total = total + 1
-            # row["rdv_site_web"] = fix_scrap_urls(row["rdv_site_web"])
-            yield row
-        logger.info("Nombre d'urls manuel " + str(total))
+    logger.info("Recherche des urls manuels")
+    for entry in sheets.load(config.sheet_id, config.page_number, config.page_number):
+        yield {"rdv_site_web": fix_scrap_urls(entry["url"])}
