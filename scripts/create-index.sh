43bd3fb3f22bbb354923a8c16fead23b7cb1025c
Florent Jaby <florent.jaby@gmail.com>
2021-05-16 13:01:58 +0200

    Génère un fichier index.html qui référence les JSON exposés

---
+++ scripts/create-index.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+cd $1
+
+find -type f -name '*.json' \
+  | cut -c3- \
+  | xargs du -h \
+  | awk 'BEGIN { print "<p>Ressources disponibles</p>\n<ul>"}
+               { print "  <li><a href=\"" $2 "\">"$2" ("$1")</a></li>" }
+         END   { print "</ul>" }' \
+  > index.html
