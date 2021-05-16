4f3aa345bb91b77bf8f67a5546474261bccc3589
Florent Jaby <florent.jaby@gmail.com>
2021-05-16 14:12:26 +0200

    Sort index.html entries

--- scripts/create-index.sh
+++ scripts/create-index.sh
@@ -6,6 +6,7 @@ cd $1
 
 find -type f -name '*.json' \
   | cut -c3- \
+  | sort \
   | xargs du -h \
   | awk 'BEGIN { print "<p>Ressources disponibles</p>\n<ul>"}
                { print "  <li><a href=\"" $2 "\">"$2" ("$1")</a></li>" }
