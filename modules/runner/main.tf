b882ab0c7692440f84333cbd8c916b29efecf926
Florent Jaby <florent.jaby@gmail.com>
2021-05-26 00:15:09 +0200

    Move provisionning file into repository

--- modules/runner/main.tf
+++ modules/runner/main.tf
@@ -17,31 +17,20 @@ resource "openstack_compute_instance_v2" "runner" {
   name            = "${var.name}-${count.index}-${var.ovh_region}"
   security_groups = ["default"]
   key_pair        = openstack_compute_keypair_v2.runner_keypair.name
-  user_data       = <<EOP
-#!/bin/bash
-export GITLAB_RUNNER_TOKEN=${var.gitlab_runner_token}
-export RUNNER_LOCATION="OVH ${var.ovh_region} (num ${count.index})"
-export TAG_LIST="ovh,ovh-${var.ovh_region},${formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())}"
-export GITLAB_RUN_UNTAGGED=yes
-curl ${local.provision_script_url} > /tmp/provision.sh
-bash /tmp/provision.sh
-EOP
 
   lifecycle {
     ignore_changes = [image_id, user_data]
     create_before_destroy = false
   }
-
 }
 
-resource null_resource "unregister" {
+resource null_resource "register_runner" {
   count = var.nb_instances
   triggers = {
     private_key = openstack_compute_keypair_v2.runner_keypair.private_key
     host = openstack_compute_instance_v2.runner[count.index].access_ip_v4
     user = "debian"
   }
-
   connection {
     user = self.triggers.user
     private_key = self.triggers.private_key
@@ -49,12 +38,20 @@ resource null_resource "unregister" {
     timeout = "45s"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-        "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do echo -e \"\\033[1;36mWaiting for cloud-init...\"; sudo tail -n 5 /var/log/cloud-init-output.log; sleep 10; done"
-    ]
+  provisioner "file" {
+    destination = "/tmp/provision.sh"
+    source = "./provision.sh"
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "export GITLAB_RUNNER_TOKEN=${var.gitlab_runner_token}",
+      "export RUNNER_LOCATION='OVH ${var.ovh_region} (num ${count.index})'",
+      "export TAG_LIST='ovh-${var.ovh_region},${formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())}'",
+      "export GITLAB_RUN_UNTAGGED=yes",
+      "sudo -E bash /tmp/provision.sh"
+    ]
+  }
 
   provisioner "remote-exec" {
     when = destroy
