4b9096be2899f5ad22f266ff3f39b18c1b5762cd
Sacha Guilhaumou <31167642+grubounet@users.noreply.github.com>
2021-07-06 12:26:28 +0200

    [Keldoc] - Circuit breaker passage 40s

--- scraper/keldoc/keldoc.py
+++ scraper/keldoc/keldoc.py
@@ -23,8 +23,8 @@ logger = logging.getLogger("scraper")
 
 
 # Allow 10 bad runs of keldoc_slot before giving up for the 60 next tries
-//@ShortCircuit("keldoc_slot", trigger=10, release=200, time_limit=20.0)
-//@Profiling.measure("keldoc_slot")
+@ShortCircuit("keldoc_slot", trigger=10, release=200, time_limit=40.0)
+@Profiling.measure("keldoc_slot")
 def fetch_slots(request: ScraperRequest, creneau_q=DummyQueue()):
     if "www.keldoc.com" in request.url:
         logger.debug(f"Fixing wrong hostname in request: {request.url}")
