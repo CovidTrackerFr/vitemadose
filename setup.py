3410a8afd43cb0a55f5fe810300e1ceed6237223
grubounet <sacha.guilhaumou@free.fr>
2021-07-31 11:11:40 +0200

    add missing distribution

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         "pydantic==1.8.2",
         "diskcache==5.2.1",
         "dotmap==1.3.23",
-        "cachecontrol==0.12.6"
+        "cachecontrol==0.12.6",
+        "lockfile==0.12.2"
     ],
 )
