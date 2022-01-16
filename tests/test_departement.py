69e68452a807bb17dc7b31b62b3f620fa667878c
Nicolas Humblot <contact@nicolashumblot.fr>
2022-01-16 13:10:44 +0100

    #659 Update insee code to department mapping table

--- tests/test_departement.py
+++ tests/test_departement.py
@@ -20,11 +20,15 @@ def test_insee_to_departement_code():
     right_insee_code = "01001"
     short_insee_code = "1001"
     DOM_TOM_insee_code = "97234"
-    passed_linked_to_Guadeloupe_insee_code = "97801"
+    passed_linked_to_Guadeloupe_insee_code = "97120"
     corse_insee_code = "2A004"
     monaco_insee_code = "99138"
     wrong_insee_code = "123"
     not_in_insee_code_table_insee_code = "12345"
+    saint_barthelemy_insee_code = "97701"
+    miquelon_laglande_insee_code = "97501"
+    saint_pierre_insee_code = "97502"
+    saint_martin_insee_code = "97801"
 
     assert departementUtils.to_departement_number(right_insee_code) == right_insee_code[:2]
     assert departementUtils.to_departement_number(short_insee_code) == short_insee_code.zfill(5)[:2]
@@ -36,6 +40,10 @@ def test_insee_to_departement_code():
         departementUtils.to_departement_number(wrong_insee_code)
     with pytest.raises(ValueError):
         departementUtils.to_departement_number(not_in_insee_code_table_insee_code)
+    assert departementUtils.to_departement_number(saint_barthelemy_insee_code) == "om"
+    assert departementUtils.to_departement_number(miquelon_laglande_insee_code) == "om"
+    assert departementUtils.to_departement_number(saint_pierre_insee_code) == "om"
+    assert departementUtils.to_departement_number(saint_martin_insee_code) == "om"
 
 
 def test_get_city():
